3e713fbc10919eab1d6ccff72e7b5994e2032f4f
Mitch Butler <mitchellbutler@gmail.com>
2023-02-03 11:10:16 +0100

    [feat] handle new responsive font sizes approach

--- packages/web/src/props/FontSize/get.ts
+++ packages/web/src/props/FontSize/get.ts
@@ -1,15 +1,27 @@
 import { DefaultTheme } from 'styled-components'
 import { FontSizeProps } from '@shallot-ui/theme'
 
-import makeResponsiveSize from './utils/makeResponsiveSize'
-import makeStaticSize from './utils/makeStaticSize'
-
 export const getFontSize =
   ({ fontSize = 'md' }: FontSizeProps = {}) =>
   ({ theme }: { theme: DefaultTheme }) => {
     const { fontSizes, breakpoints } = theme
-    const size = fontSizes[fontSize] as number | number[]
-    return Array.isArray(size)
-      ? makeResponsiveSize(size, breakpoints)
-      : makeStaticSize(size)
+    const size = fontSizes[fontSize]
+
+    // The default font size will come from the theme's `fontSizes` object.
+    // It will be overridden by any `fontSizes` modifiers defined in the theme's
+    // `breakpoints` configuration.
+    let style = `font-size: ${size}px;`
+
+    // Loop through the breakpoints and add any modifiers to the font sizes.
+    // e.g., breakpoints: { 960: { fontSizes: { md: 16 }}}
+    Object.entries(breakpoints).map(([width, modifiers]) => {
+      const sizeModifier = modifiers.fontSizes?.[fontSize]
+      if (sizeModifier) {
+        style += `@media (min-width: ${width}px) {
+          font-size: ${sizeModifier}px;
+        }`
+      }
+    })
+
+    return style
   }
