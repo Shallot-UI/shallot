86e9de71f8c6cf5fd20718d7d4b80375f026899a
Mitch Butler <mitchellbutler@gmail.com>
2023-02-03 10:59:39 +0100

    [feat] moves theme breakpoints to an object

--- packages/theme/src/theme/breakpoints/index.ts
+++ packages/theme/src/theme/breakpoints/index.ts
@@ -1,1 +1,13 @@
-export const DEFAULT_BREAKPOINTS: number[] = [960]
+export const DEFAULT_BREAKPOINTS = [
+  {
+    960: {
+      fontSizes: {
+        xl: 80,
+        lg: 36,
+        md: 16,
+        sm: 14,
+        xs: 12,
+      },
+    },
+  },
+]
--- packages/theme/src/theme/fontSizes/index.ts
+++ packages/theme/src/theme/fontSizes/index.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_FONT_SIZES = {
-  xl: [54, 80],
-  lg: [24, 36],
-  md: [14, 16],
-  sm: [12, 14],
-  xs: [10, 12],
+  xl: 54,
+  lg: 24,
+  md: 14,
+  sm: 12,
+  xs: 10,
 } as const
