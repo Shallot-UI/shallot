9478136db4f6e7a6dedee28ff7f3e0dcdaf89568
Mitch Butler <mitchellbutler@gmail.com>
2023-02-04 18:02:30 +0100

    [feat] adding native formik

---
+++ packages/native-formik/src/components/FormikInput/index.tsx
@@ -0,0 +1,27 @@
+import { FunctionComponent } from 'react'
+import { useField } from 'formik'
+import {
+  FormInput,
+  FormInputProps,
+  FormInputStyleProps,
+} from '@shallot-ui/native'
+
+export type FormikInputProps = Omit<FormInputProps, 'value' | 'setValue'> &
+  FormInputStyleProps & { name: string }
+
+export const FormikInput: FunctionComponent<FormikInputProps> = ({
+  name,
+  ...rest
+}) => {
+  const [field, meta] = useField(name)
+
+  return (
+    <FormInput
+      {...field}
+      errorText={meta.touched && meta.error ? meta.error : undefined}
+      valid={meta.touched && !meta.error}
+      name={name}
+      {...rest}
+    />
+  )
+}
