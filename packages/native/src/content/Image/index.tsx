c477657d7756218cb109780baaa5f7a0f786bbd8
Mitch Butler <mitchellbutler@gmail.com>
2023-05-12 16:14:06 -0700

    [fix] implements native image

--- packages/native/src/content/Image/index.tsx
+++ packages/native/src/content/Image/index.tsx
@@ -1,19 +1,26 @@
 import { FunctionComponent } from 'react'
-import { useTheme } from 'styled-components/native'
+import styled, { DefaultTheme } from 'styled-components/native'
 import FastImage, { FastImageProps } from 'react-native-fast-image'
-import { RadiusProps, SizingProps, UnitsAroundProps } from '@shallot-ui/theme'
-
-import { Column } from '../../containers'
+import { ShallotProp, getStyle, getUnits, getRadius } from '@shallot-ui/core'
 
 type FastImageComponentProps = Omit<FastImageProps, 'source'> & {
   source: FastImageProps['source'] | undefined
 }
 
-export interface ImageProps
-  extends FastImageComponentProps,
-    SizingProps,
-    RadiusProps,
-    UnitsAroundProps {}
+export interface ImageProps extends FastImageComponentProps {
+  shallot?: ShallotProp
+  radius?: keyof DefaultTheme['radii']
+  unitHeight?: number
+  unitWidth?: number
+  unitsAround?: number
+  unitsAbove?: number
+  unitsBelow?: number
+  unitsLeft?: number
+  unitsRight?: number
+}
+
+const Container = styled.View(getStyle)
+const InnerImage = styled(FastImage)(getStyle)
 
 export const Image: FunctionComponent<ImageProps> = (props) => {
   const {
@@ -28,29 +35,29 @@ export const Image: FunctionComponent<ImageProps> = (props) => {
     ...rest
   } = props
 
-  const theme = useTheme()
-
-  // If the user has specified a unitWidth, we need to convert it to a pixel
-  // width for the FastImage component.
-  const width = unitWidth && unitWidth * theme.gridUnits[0]
-
-  // If the user has specified a unitHeight, we need to convert it to a pixel
-  // height for the FastImage component.
-  const height = unitHeight && unitHeight * theme.gridUnits[0]
-
   return (
-    <Column
-      unitHeight={unitHeight}
-      unitWidth={unitWidth}
-      radius={radius}
-      unitsAround={unitsAround}
-      unitsAbove={unitsAbove}
-      unitsBelow={unitsBelow}
-      unitsLeft={unitsLeft}
-      unitsRight={unitsRight}
-      style={{ overflow: 'hidden' }}
+    <Container
+      shallot={{
+        ...(unitHeight && { height: getUnits(unitHeight) }),
+        ...(unitWidth && { width: getUnits(unitWidth) }),
+        ...(radius && { borderRadius: getRadius(radius) }),
+        ...(unitsAround && { margin: getUnits(unitsAround) }),
+        ...(unitsAbove && { marginTop: getUnits(unitsAbove) }),
+        ...(unitsBelow && { marginBottom: getUnits(unitsBelow) }),
+        ...(unitsLeft && { marginLeft: getUnits(unitsLeft) }),
+        ...(unitsRight && { marginRight: getUnits(unitsRight) }),
+        overflow: 'hidden',
+      }}
     >
-      {rest.source && <FastImage style={{ width, height }} {...rest} />}
-    </Column>
+      {rest.source && (
+        <InnerImage
+          shallot={{
+            ...(unitWidth && { width: getUnits(unitWidth) }),
+            ...(unitHeight && { height: getUnits(unitHeight) }),
+          }}
+          {...rest}
+        />
+      )}
+    </Container>
   )
 }
