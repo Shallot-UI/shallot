a02dbf14aa2db945cc5c1458bb334994ba89c1a5
Aaron Thomas <aarthomas01@gmail.com>
2023-07-11 09:08:18 -0400

    add ReactNode and ReactNode[] as children on Column, Row, and Box components

--- components/Column/types.ts
+++ components/Column/types.ts
@@ -10,6 +10,7 @@ import {
   SizingProps,
   TextColorProps,
 } from '@shallot-ui/core'
+import { ReactNode } from 'react'
 
 export type ColumnStyleProps = AlignmentProps &
   BorderProps &
@@ -25,7 +26,11 @@ export type ColumnShallot = ShallotProp & {
   // We use `flexDirection` to infer the `alignItems` and `justifyContent` props
   // for the `Column` component, so it can't be a mixin prop.
   flexDirection?: 'column' | 'column-reverse' | 'row' | 'row-reverse'
+  children: ReactNode | ReactNode[]
 }
 
 export type ColumnProps<T = {}> = T &
-  ColumnStyleProps & { shallot?: ColumnShallot }
+  ColumnStyleProps & {
+    shallot?: ColumnShallot
+    children: ReactNode | ReactNode[]
+  }
