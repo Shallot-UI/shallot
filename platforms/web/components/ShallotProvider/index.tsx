bbbe8a7f87bc065042e7b135bf73b7655aea3add
Aaron Thomas <aarthomas01@gmail.com>
2023-07-28 10:55:03 -0400

    add ShallotProvider Component

---
+++ platforms/web/components/ShallotProvider/index.tsx
@@ -0,0 +1,13 @@
+import { makeTheme } from '@shallot-ui/theme'
+import { ReactNode } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { Theme } from '../../styled'
+
+type ShallotProviderProps = {
+  children: ReactNode | ReactNode[]
+  theme: Theme
+}
+
+export const ShallotProvider = ({ children, theme }: ShallotProviderProps) => {
+  return <ThemeProvider theme={makeTheme(theme)}>{children}</ThemeProvider>
+}
