aa83a9a5a17f0eb60e2928c294eb21419eb15cb9
Aaron Thomas <aarthomas01@gmail.com>
2023-07-28 11:41:16 -0400

    add a default value for theme and make it an optional prop

--- platforms/web/components/ShallotProvider/index.tsx
+++ platforms/web/components/ShallotProvider/index.tsx
@@ -5,9 +5,12 @@ import { Theme } from '../../styled'
 
 type ShallotProviderProps = {
   children: ReactNode | ReactNode[]
-  theme: Theme
+  theme?: Theme
 }
 
-export const ShallotProvider = ({ children, theme }: ShallotProviderProps) => {
+export const ShallotProvider = ({
+  children,
+  theme = {},
+}: ShallotProviderProps) => {
   return <ThemeProvider theme={makeTheme(theme)}>{children}</ThemeProvider>
 }
