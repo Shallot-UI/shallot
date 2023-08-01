673e705de9756b9c02ba93afc6dd16ee76c0fe4a
Aaron Thomas <aarthomas01@gmail.com>
2023-08-01 15:32:26 -0400

    edit default `boxShadow` values

--- packages/theme/src/theme/shadows/index.ts
+++ packages/theme/src/theme/shadows/index.ts
@@ -1,24 +1,14 @@
 import { DEFAULT_COLORS } from '../colors'
 
 export const DEFAULT_SHADOWS = {
-  pressable: {
-    transform: 'translateY(0)',
-    boxShadow: '0 4px 6px -4px rgba(0,0,0,0.4)',
-  },
-  hover: {
-    boxShadow: '0 4px 12px -4px rgba(0,0,0,0.4)',
-    transform: 'translateY(-2px)',
-  },
-  pressed: {
-    boxShadow: '0 0 0 -4px rgba(0,0,0,0)',
-    transform: 'translateY(0)',
-  },
-  focused: {
-    boxShadow: `0 0 0 4px ${DEFAULT_COLORS.Primary[300]}29`,
-    transform: 'translateY(0)',
-  },
-  none: {
-    boxShadow: 'none',
-    transform: 'none',
-  },
+  0: 'none',
+  100: 'rgba(0, 0, 0, 0.08) 0 1px 1px',
+  200: 'rgba(0, 0, 0, 0.14) 0 2px 2px',
+  300: 'rgba(0, 0, 0, 0.18) 0 4px 4px',
+  400: 'rgba(0, 0, 0, 0.22) 0 8px 8px',
+  500: 'rgba(0, 0, 0, 0.26) 0 12px 24px',
+  600: 'rgba(0, 0, 0, 0.3) 0 16px 32px',
+  700: 'rgba(0, 0, 0, 0.4) 0 20px 40px',
+  800: 'rgba(0, 0, 0, 0.5) 0 24px 48px',
+  900: 'rgba(0, 0, 0, 0.6) 0 28px 56px',
 }
