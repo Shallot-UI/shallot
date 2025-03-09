bc8fc58bfc2775d552438385f581e979ab4dade6
Mitchell Butler <mitchellbutler@gmail.com>
2025-03-09 10:22:25 -0400

    feat: adds react-email

--- packages/email/index.ts
+++ packages/email/index.ts
@@ -1,11 +1,3 @@
-// Components
-export * from '@shallot-ui/component-button'
-export * from '@shallot-ui/component-checkbox'
-export * from '@shallot-ui/component-input'
-export * from '@shallot-ui/component-modal'
-export * from '@shallot-ui/component-switch'
-export * from '@shallot-ui/component-tag'
-
 // Core
 export * from '@shallot-ui/core-mixins'
 export * from '@shallot-ui/core-props'
---
+++ platforms/react-email/styled.d.ts
@@ -0,0 +1,12 @@
+import 'styled-components'
+
+import { DEFAULT_REACT_THEME } from './src'
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    mode?: string
+    tokens: (typeof DEFAULT_REACT_THEME)['tokens']
+    variants: (typeof DEFAULT_REACT_THEME)['variants']
+    globals: (typeof DEFAULT_REACT_THEME)['globals']
+  }
+}
