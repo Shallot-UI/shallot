39ff93bdd24b52db1186bdbb89328f04ed333a92
Mitchell Butler <mitchellbutler@gmail.com>
2025-07-14 08:12:44 -0400

    feat: explicitly export csstype

--- platforms/nextjs/src/index.ts
+++ platforms/nextjs/src/index.ts
@@ -2,3 +2,5 @@ import type {} from 'csstype'
 
 export * from './components'
 export * from './theme'
+
+export type { CSS } from '@shallot-ui/core-theme'
--- platforms/react/src/index.ts
+++ platforms/react/src/index.ts
@@ -6,3 +6,5 @@ export * from './context'
 export * from './styles'
 export * from './utils'
 export * from './theme'
+
+export type { CSS } from '@shallot-ui/core-theme'
